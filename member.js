e69f1133068e0bbb4622bb6de96d15f90a5552b3
Paul Sola-Eniolawun <70896150+Musecreatives@users.noreply.github.com>
2023-11-17 06:13:19 +0100

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var x = document.getElementById("member");
+    var y = document.getElementById("skills");
+    var z = document.getElementById("btn");
+
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
